2544c710c5315c135659bb4cef64e509863a4ab8
kaili-yang <79012058+kaili-yang@users.noreply.github.com>
2022-09-20 23:57:43 -0700

    leetcode 985,864

---
+++ 854. K-Similar Strings.java
@@ -0,0 +1,49 @@
+// 854. K-Similar Strings
+// 官方答案
+
+class Solution {
+    public int kSimilarity(String s1, String s2) {
+        int n = s1.length();
+        Queue<Pair<String, Integer>> queue = new ArrayDeque<Pair<String, Integer>>();
+        Set<String> visit = new HashSet<String>();
+        queue.offer(new Pair<String, Integer>(s1, 0));
+        visit.add(s1);
+        int step = 0;
+        while (!queue.isEmpty()) {
+            int sz = queue.size();
+            for (int i = 0; i < sz; i++) {
+                Pair<String, Integer> pair = queue.poll();
+                String cur = pair.getKey();
+                int pos = pair.getValue();
+                if (cur.equals(s2)) {
+                    return step;
+                }
+                while (pos < n && cur.charAt(pos) == s2.charAt(pos)) {
+                    pos++;
+                }
+                for (int j = pos + 1; j < n; j++) {
+                    if (s2.charAt(j) == cur.charAt(j)) {
+                        continue;
+                    }
+                    if (s2.charAt(pos) == cur.charAt(j)) {
+                        String next = swap(cur, pos, j);
+                        if (!visit.contains(next)) {
+                            visit.add(next);
+                            queue.offer(new Pair<String, Integer>(next, pos + 1));
+                        }
+                    }
+                }
+            }
+            step++;
+        } 
+        return step;
+    }
+
+    public String swap(String cur, int i, int j) {
+        char[] arr = cur.toCharArray();
+        char c = arr[i];
+        arr[i] = arr[j];
+        arr[j] = c;
+        return new String(arr);
+    }
+}
