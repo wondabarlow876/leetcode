2dbe4fb568f1561b2b6c2aa027c762db621992c6
kaili-yang <79012058+kaili-yang@users.noreply.github.com>
2022-10-27 22:09:04 -0700

    Monotonic Stack

---
+++ 496. Next Greater Element I.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int m = nums1.length, n = nums2.length;
+        int[] res = new int[m];
+        for (int i = 0; i < m; ++i) {
+            int j = 0;
+            while (j < n && nums2[j] != nums1[i]) {
+                ++j;
+            }
+            int k = j + 1;
+            while (k < n && nums2[k] < nums2[j]) {
+                ++k;
+            }
+            res[i] = k < n ? nums2[k] : -1;
+        }
+        return res;
+    }
+}
